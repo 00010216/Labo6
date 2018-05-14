afc51e3644281f79dd3459c8f11b55b3dfa8bfd0
00010216 <00010216@uca.edu.sv>
2018-05-13 20:59:50 -0600

    agregando clase para administrar el fragmento del item 1 de ejemplo

--- app/src/main/java/com/example/kcruz/labo6drawer/RestaurantIntroductionFragment.java
+++ app/src/main/java/com/example/kcruz/labo6drawer/RestaurantIntroductionFragment.java
@@ -1,4 +1,22 @@
 package com.example.kcruz.labo6drawer;
 
-public class RestaurantIntroductionFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class RestaurantIntroductionFragment extends Fragment {
+
+    TextView newFragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_information_view, container,false);
+
+        newFragment = view.findViewById(R.id.txt_title);
+
+        return view;
+    }
 }
