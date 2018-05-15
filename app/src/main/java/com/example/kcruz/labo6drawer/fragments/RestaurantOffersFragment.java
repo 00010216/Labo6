4235ec46037eaf831a369ac041beb266f8e831c6
00010216 <00010216@uca.edu.sv>
2018-05-14 21:52:56 -0600

    se agrego el fragmento para administrar las ofertas

--- app/src/main/java/com/example/kcruz/labo6drawer/fragments/RestaurantOffersFragment.java
+++ app/src/main/java/com/example/kcruz/labo6drawer/fragments/RestaurantOffersFragment.java
@@ -1,4 +1,22 @@
 package com.example.kcruz.labo6drawer.fragments;
 
-public class RestaurantOffersFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.kcruz.labo6drawer.R;
+
+public class RestaurantOffersFragment extends Fragment{
+    TextView newFragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_offers, container,false);
+
+        newFragment = view.findViewById(R.id.txt_title);
+
+        return view;
+    }
 }
